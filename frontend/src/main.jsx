7f9c42da7d56370b10722996db2315a717700fc7
Muhammad Harib <haribharry7@gmail.com>
2026-05-09 18:33:47 +0500

    updated and comment out the datarail in lighttheme and updated main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import './index.css'
 import './index.light-theme-override.css'  // ← add this line
 
 // react-grid-layout drag/resize styles
